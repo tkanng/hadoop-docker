9fcbafec70985231f30140fca5df133502c160c3
tkanng <tkanng@192.168.124.4>
2019-07-10 23:17:37 +0800

    hh

--- start-hive.sh
+++ start-hive.sh
@@ -6,3 +6,12 @@ sudo docker run -itd -p 10000:8000 \
                 --name hive \
                 --hostname hive \
                 $Image &> /dev/null
+
+
+echo "Initialize schema: "
+echo "cd \$HIVE_HOME && bin/schematool -dbType derby -initSchema"
+echo "Debug hive: "
+echo "cd \$HIVE_HOME && bin/hive  --debug:port=8001  -hiveconf hive.root.logger=DEBUG,console"
+
+echo "Entering hive container......"
+sudo docker exec -it hive bash
