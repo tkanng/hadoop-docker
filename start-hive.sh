e35c6ba9756a8767877e2cc5358ee7d590426fa9
tkanng <tkanng@gmail.com>
2019-07-10 23:11:16 +0800

    j

--- start-hive.sh
+++ start-hive.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 Image=tkanng/hive:3.1.1
-
-# 容器8000端口暴露至宿主机10000端口
-sudo docker run -itd \
+sudo docker run -itd -p 10000:8000 \
                 --net=hadoop \
                 --name hive \
                 --hostname hive \
-                -p 10001:8000 \  
                 $Image &> /dev/null
